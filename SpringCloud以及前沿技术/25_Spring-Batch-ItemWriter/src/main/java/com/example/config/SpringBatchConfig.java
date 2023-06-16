4ebe94d4b5e3777af6069c1408fb0291e3bdbba7
游诗成 <youshicheng97@163.com>
2023-06-16 17:53:58 +0800

    no message

--- SpringCloud以及前沿技术/25_Spring-Batch-ItemWriter/src/main/java/com/example/config/SpringBatchConfig.java
+++ SpringCloud以及前沿技术/25_Spring-Batch-ItemWriter/src/main/java/com/example/config/SpringBatchConfig.java
@@ -1,36 +1,37 @@
 package com.example.config;
 
 import com.example.dto.User;
-import com.example.processor.CustomizeItemProcessor;
-import com.example.service.Impl.UserServiceImpl;
+import com.example.prepared.UserPreStatementSetter;
 import jakarta.annotation.Resource;
-import lombok.NonNull;
-import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.Chunk;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.adapter.ItemProcessorAdapter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.support.CompositeItemProcessor;
-import org.springframework.batch.item.support.ScriptItemProcessor;
-import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
+import org.springframework.batch.item.json.JsonFileItemWriter;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
+import org.springframework.batch.item.support.CompositeItemWriter;
+import org.springframework.batch.item.support.builder.CompositeItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.PathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.script.ScriptEngineManager;
+import javax.sql.DataSource;
 import java.util.Arrays;
 
 /**
  * @author 游家纨绔
- * @dateTime 2023-06-01 22:38
+ * @dateTime 2023-06-16 08:51
  * @apiNote TODO SpringBatch配置类
  */
 @Configuration
@@ -43,9 +44,6 @@ public class SpringBatchConfig {
 
 
     // 读取文件 user-validation.txt 数据
-    // TODO ValidatingItemProcessor: 校验处理器 (校验name值不为null和空值)
-    //  BeanValidatingItemProcessor 是 ValidatingItemProcessor 子类
-    //  核心 BeanValidatingItemProcessor 类是Spring Batch提供现成的Validator校验类，直接使用即可
     @Bean
     public FlatFileItemReader<User> itemReader1(){
         return new FlatFileItemReaderBuilder<User>()
@@ -55,41 +53,113 @@ public class SpringBatchConfig {
                 // 解析数据 -- 指定解析其使用#分割 -- 默认是，逗号
                 .delimited().delimiter("#")
                 // 按照#截取数据之后，数据怎么命名
-                .names("Id", "name", "age")
+                .names("id", "name", "age")
                 // 封装数据 -- 将读取的数据封装到对象：User对象
                 .targetType(User.class) // 自动封装
                 .build();
     }
-    // 校验参数是否合法，不合法则丢弃
     @Bean
-    public BeanValidatingItemProcessor<User> beanValidatingItemProcessor(){
-        BeanValidatingItemProcessor<User> itemProcessor = new BeanValidatingItemProcessor<>();
-        itemProcessor.setFilter(true);
-        return itemProcessor;
-    }
-    @Bean
-    public ItemWriter<User> itemWriter1() {
-        return new ItemWriter<User>() {
-            @Override
-            public void write(@NonNull Chunk<? extends User> chunk) throws Exception {
-                chunk.getItems().forEach(System.out::println);
-            }
-        };
+    public FlatFileItemWriter<User> itemWriter1() {
+        return new FlatFileItemWriterBuilder<User>()
+                .name("userFlatItemWriter")
+                // 输出位置
+                .resource(new PathResource("/Users/youshicheng/IDEA/java-layout-path/SpringCloud以及前沿技术/25_Spring-Batch-ItemWriter/FlatFileItemWriter.txt"))
+                .formatted()
+                // 输出数据形式
+                .format("id: %s, 姓名: %s, 年龄: %s")
+                // 需要输出的属性
+                .names("id", "name", "age")
+                .build();
     }
     @Bean
     public Step step1() {
         return new StepBuilder("step1", jobRepository)
                 .<User, User>chunk(1, batchTransactionManager)
                 .reader(itemReader1())
-                // 这里多了一步操作，以便引入ItemProcessor组件springboot
-                .processor(beanValidatingItemProcessor())
                 .writer(itemWriter1())
                 .build();
     }
     @Bean
     public Job job1() {
-        return new JobBuilder("Validating Processor Job1", jobRepository)
+        return new JobBuilder("FlatFile Writer Job", jobRepository)
                 .start(step1())
+                .build();
+    }
+
+    // TODO 输出json文件
+    @Bean
+    public JsonFileItemWriter<User> itemWriter2() {
+        return new JsonFileItemWriterBuilder<User>()
+                .name("userJsonItemWriter")
+                // 输出位置
+                .resource(new PathResource("/Users/youshicheng/IDEA/java-layout-path/SpringCloud以及前沿技术/25_Spring-Batch-ItemWriter/JsonFileItemWriter.json"))
+                // json对象调度器--将user对象缓存json格式，输出文档中
+                .jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
+                .build();
+    }
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .<User, User>chunk(1, batchTransactionManager)
+                .reader(itemReader1())
+                .writer(itemWriter2())
+                .build();
+    }
+    @Bean
+    public Job job2() {
+        return new JobBuilder("JsonFile Writer Job", jobRepository)
+                .start(step2())
+                .build();
+    }
+
+    // TODO 输出到数据库
+    @Resource
+    private DataSource dataSource;
+    @Bean
+    public JdbcBatchItemWriter<User> itemWriter3(){
+        return new JdbcBatchItemWriterBuilder<User>()
+                .dataSource(dataSource)
+                .sql("insert into user(id, name, age) values(?, ?, ?)")
+                // 设置sql中占位符参数
+                .itemPreparedStatementSetter(new UserPreStatementSetter())
+                .build();
+    }
+    @Bean
+    public Step step3() {
+        return new StepBuilder("step3", jobRepository)
+                .<User, User>chunk(1, batchTransactionManager)
+                .reader(itemReader1())
+                .writer(itemWriter3())
+                .build();
+    }
+    @Bean
+    public Job job3() {
+        return new JobBuilder("DataSource Writer Job", jobRepository)
+                .start(step3())
+                .build();
+    }
+
+    // TODO 输出多终端
+    //  上面几种输出方法都是一对一，真实开发可能没那么简单了，可能存在一对多，多个终端输出
+    //  例如现在要求将 user-validation.txt 文件数据读取出来，输出到 FlatFileItemWriter.txt / JsonFileItemWriter.json / 数据库user表中
+    @Bean
+    public CompositeItemWriter<User> compositeItemWriter(){
+        return new CompositeItemWriterBuilder<User>()
+                .delegates(Arrays.asList(itemWriter1(), itemWriter2(), itemWriter3()))
+                .build();
+    }
+    @Bean
+    public Step step4() {
+        return new StepBuilder("step4", jobRepository)
+                .<User, User>chunk(1, batchTransactionManager)
+                .reader(itemReader1())
+                .writer(compositeItemWriter())
+                .build();
+    }
+    @Bean
+    public Job job4() {
+        return new JobBuilder("Composite Writer Job", jobRepository)
+                .start(step4())
                 .build();
     }
 }
---
+++ SpringCloud以及前沿技术/25_Spring-Batch-ItemWriter/src/main/java/com/example/dto/User.java
@@ -0,0 +1,24 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 游家纨绔
+ * @dateTime 2023-06-02 18:20
+ * @apiNote TODO 用户类
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private long id;
+    @NotBlank(message = "用户名不能为null或空值")
+    private String name;
+    private int age;
+}
