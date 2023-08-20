2444e38513f20b80ac4ecdba573ede208224cb9d
游诗成 <youshicheng97@163.com>
2023-08-21 07:05:38 +0800

    小版本提交，无特殊重大功能

--- Java成神之路/Java基础及进阶/O15JUC进阶理解/H对JMM的理解/B单例模式详解.java
+++ Java成神之路/Java基础及进阶/O15JUC进阶理解/H对JMM的理解/B单例模式详解.java
@@ -21,6 +21,7 @@ package O15JUC进阶理解.H对JMM的理解;
  *     3.确保对象实例只有一个。
  *         只对类进行一次实例化，以后都直接获取第一次实例化的对象。     
  */
+@SuppressWarnings({"NonAsciiCharacters", "InstantiationOfUtilityClass"})
 public class B单例模式详解 {
     /**
      * 饿汉式单例
@@ -43,14 +44,15 @@ public class B单例模式详解 {
 
 /**
  * 线程不安全的懒汉式单例
- *    顾名思义就是实例在用到的时候才去创建，“比较懒”，用的时候才去检查有没有实例，如果有则返回，没有则新建。
+ *    顾名思义就是实例在用到的时候才去创建，"比较懒"，用的时候才去检查有没有实例，如果有则返回，没有则新建。
  *    有线程安全和线程不安全两种写法，区别就是synchronized关键字。
  *
  * 思考一下：  为什么会线程不安全？
  *           比如有两个线程a,b。都在其run()方法中调用了懒汉式单例的静态方法getInstance()
- *           线程a会发现lazyMan==null,就会去实例化对象，得到LazyMan@5643x
- *           线程b在线程a实例化对象时，也发现了lazyMan==null，也去实例化对象，得到LazyMan@4564x
+ *           线程a会发现 lazyMan == null,就会去实例化对象，得到LazyMan@5643x
+ *           线程b在线程a实例化对象时，也发现了lazyMan  == null，也去实例化对象，得到LazyMan@4564x
  */
+@SuppressWarnings("InstantiationOfUtilityClass")
 class LazyMan{
     private LazyMan(){
 
@@ -68,12 +70,12 @@ class LazyMan{
     public static void main(String[] args) {
         new Thread(() -> {
             for (int i = 0; i < 10; i++) {
-                System.out.println(LazyMan.getInstance());
+                System.out.println(Thread.currentThread().getName() + "--" + LazyMan.getInstance());
             }
         }).start();
 
         new Thread(() -> {
-            System.out.println(LazyMan.getInstance());
+            System.out.println(Thread.currentThread().getName() + "--" + LazyMan.getInstance());
         }, "b").start();
     }
 }
@@ -87,9 +89,8 @@ class Singleton{
     public String getStr() {
         return str;
     }
-
     private Singleton(){
-        str="hello";
+        str = "hello";
     }
 
     private static volatile Singleton singleton; // 第二层锁，volatile关键字禁止指令重排
@@ -101,10 +102,13 @@ class Singleton{
                 if(singleton == null){ // 第二层检查。假设没有第二层检查，那么第一个线程创建完对象释放锁后，后面进入对象也会创建对象，会产生多个对象
                     // volatile关键字作用为禁止指令重排，保证返回Singleton对象一定在创建对象后
                     singleton = new Singleton();
-                    // singleton=new Singleton语句为非原子性，实际上会执行以下内容：
+                    // singleton = new Singleton语句为非原子性，实际上会执行以下内容：
                     // (1)在堆上开辟空间；(2)属性初始化；(3)引用指向对象
-                    // 假设以上三个内容为三条单独指令，因指令重排可能会导致执行顺序为1->3->2(正常为1->2->3),当单例模式中存在普通变量需要在构造方法中进行初始化操作时，单线程情况下，顺序重排没有影响；但在多线程情况下，假如线程1执行singleton=new Singleton()语句时先1再3，由于系统调度线程2的原因没来得及执行步骤2，但此时已有引用指向对象也就是singleton!=null，故线程2在第一次检查时不满足条件直接返回singleton，此时singleton为null(即str值为null)
-                    // volatile关键字可保证singleton=new Singleton()语句执行顺序为123，因其为非原子性依旧可能存在系统调度问题(即执行步骤时被打断)，但能确保的是只要singleton!=0，就表明一定执行了属性初始化操作；而若在步骤3之前被打断，此时singleton依旧为null，其他线程可进入第一层检查向下执行创建对象
+                    // 假设以上三个内容为三条单独指令，因指令重排可能会导致执行顺序为1->3->2(正常为1->2->3),当单例模式中存在普通变量需要在构造方法中进行初始化操作时，
+                    // 单线程情况下，顺序重排没有影响；但在多线程情况下，假如线程1执行singleton = new Singleton()语句时先1再3，由于系统调度线程2的原因没来得及执行步骤2，
+                    // 但此时已有引用指向对象也就是singleton != null，故线程2在第一次检查时不满足条件直接返回singleton，此时singleton为null(即str值为null)
+                    // volatile关键字可保证singleton = new Singleton()语句执行顺序为123，因其为非原子性依旧可能存在系统调度问题(即执行步骤时被打断)，
+                    // 但能确保的是只要singleton != 0，就表明一定执行了属性初始化操作；而若在步骤3之前被打断，此时singleton依旧为null，其他线程可进入第一层检查向下执行创建对象
                 }
             }
         }
@@ -118,7 +122,6 @@ class Singleton{
             }
         }).start();
 
-        int i = 123;
-        System.out.println();
+        System.out.println( Singleton.getInstance().getStr() );
     }
 }
