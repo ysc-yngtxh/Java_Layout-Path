b94ba6ec14fa6031982b04b39cdcddd3ef92d5e6
游诗成 <youshicheng97@163.com>
2023-08-23 20:10:16 +0800

    小版本提交，无特殊重大功能

---
+++ Java成神之路/Java基础及进阶/O15_JUC进阶理解/JUC_Ⅰ_关于lock锁/Synchronized语句if.java
@@ -0,0 +1,75 @@
+package O15_JUC进阶理解.JUC_Ⅰ_关于lock锁;
+
+public class Synchronized语句if{
+    /**
+     * 为什么在synchronized中使用wait、notify等关键字时，尽量不使用if作为判断语句的原因.
+     *    如果在一if判断中去使用wait、notify，当前只有一两个线程去调用是没有什么问题，但是一旦很多线程去调用，那么就会出现错误(num值可能会超过1)。
+     *    if判断中的wait、notify会存在一个虚假唤醒的问题，换句话说，等待应该总是出现在while循环中。可以参考类B
+    */
+    public static void main(String[] args) {
+        Data data = new Data();
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.demo1();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"A").start();
+
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.demo2();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"B").start();
+
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.demo1();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"C").start();
+
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.demo2();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"D").start();
+
+    }
+}
+
+class Data{
+
+    int num=0;
+
+    public synchronized void demo1() throws InterruptedException {
+        if(num == 0){
+            this.wait();
+        }
+        num--;
+        System.out.println(Thread.currentThread().getName() + "---num值---" + num);
+        this.notifyAll();
+    }
+
+    public synchronized void demo2() throws InterruptedException {
+        if(num != 0){
+            this.wait();
+        }
+        num++;
+        System.out.println(Thread.currentThread().getName() + "---num值---" + num);
+        this.notifyAll();
+    }
+}
---
+++ Java成神之路/Java基础及进阶/O15_JUC进阶理解/JUC_Ⅲ_常用辅助类/Semaphore辅助类.java
@@ -0,0 +1,33 @@
+package O15_JUC进阶理解.JUC_Ⅲ_常用辅助类;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class Semaphore辅助类 {
+    /**
+     * Semaphre:信号机
+     *        semaphore.acquire() 获得，假设如果已经满了，等待，等待被释放为止
+     *        semaphore.release() 释放，会将当前的信号量释放，然后唤醒等待的线程
+     *        作用：多个共享资源互斥的使用！并发限流，控制最大的线程数
+     */
+    public static void main(String[] args) {
+        // 线程数量，停车位！限流！
+        Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 1; i <= 6; i++) {
+            new Thread(() -> {
+                try {
+                    semaphore.acquire(); // 得到
+                    System.out.println(Thread.currentThread().getName() + "抢到车位");
+                    TimeUnit.SECONDS.sleep(2);
+                    System.out.println(Thread.currentThread().getName() + "离开车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release(); // release()释放
+                }
+            }, String.valueOf(i)).start();
+        }
+
+    }
+}
