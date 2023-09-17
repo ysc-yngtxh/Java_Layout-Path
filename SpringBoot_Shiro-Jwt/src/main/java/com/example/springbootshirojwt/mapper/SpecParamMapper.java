caaad427fa8541fc4cb2a4f5bf26338fddce299d
游诗成 <youshicheng97@163.com>
2023-09-17 09:24:27 +0800

    更新

--- SpringBoot_Shiro-Jwt/src/main/java/com/example/springbootshirojwt/mapper/SpecParamMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootshirojwt.mapper;
-
-import com.example.springbootshirojwt.pojo.SpecParam;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SpecParamMapper extends Mapper<SpecParam> {
-}
--- SpringBoot_Shiro-Jwt/src/main/java/com/example/springbootshirojwt/mapper/SpuMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootshirojwt.mapper;
-
-
-import com.example.springbootshirojwt.pojo.Spu;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SpuMapper extends Mapper<Spu> {
-}
