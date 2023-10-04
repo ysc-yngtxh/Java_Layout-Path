af3267b14f7caae1cb3b4f9b36eedd8a59c8d4ce
游诗成 <youshicheng97@163.com>
2023-10-05 00:24:14 +0800

    提交更新，此次更新无重大版本变革

--- DB数据库/MySQL数据库/src/作业题.java
+++ DB数据库/MySQL数据库/src/作业题.java
@@ -6,7 +6,9 @@ MySQL作业题
 
      CREATE TABLE t_emp(
         `id` INT(20) PRIMARY KEY AUTO_INCREMENT COMMENT '主键Id',
+        `emp_no` BIGINT(20) COMMENT '员工编号',
         `ename` VARCHAR(255) COMMENT '员工姓名',
+        `mgr` BIGINT(20) COMMENT '领导编号',
         `sal` DOUBLE(10, 2) COMMENT '工资',
         `comm` DOUBLE(10, 2) COMMENT '津贴',
         `dept_no` VARCHAR(255) COMMENT '部门',
@@ -19,7 +21,14 @@ MySQL作业题
         `grade` VARCHAR(255) COMMENT '工资等级',
         `loSal` DOUBLE(10, 2) COMMENT '最低金额',
         `hiSal` DOUBLE(10, 2) COMMENT '最高金额'
-     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='员工信息表';
+     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='工资等级表';
+
+     CREATE TABLE dept(
+        `id` BIGINT(20) PRIMARY KEY AUTO_INCREMENT COMMENT '主键Id',
+        `dept_no` VARCHAR(255) COMMENT '部门编号',
+        `d_name` VARCHAR(255) COMMENT '部门名称',
+        `loc` VARCHAR(255) COMMENT '部门地址'
+     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='部门信息表';
 
      1、取得每个部门最高薪水的人员名称。
      2、哪些人的薪水在部门的平均薪水之上
@@ -56,11 +65,11 @@ MySQL作业题
         SELECT
            e.dept_no, e.name, e.sal
         FROM
-           t_emp e,
+           t_emp e
+        INNER JOIN
            (SELECT dept_no, MAX(sal) AS maxSal FROM t_emp GROUP BY dept_no) t
-        WHERE
-           e.dept_no=t.dept_no,
-           e.sal=t.maxSal;
+        ON
+           e.dept_no=t.dept_no AND e.sal=t.maxSal;
 
      2、哪些人的薪水在部门的平均薪水之上
         SELECT 
@@ -68,21 +77,19 @@ MySQL作业题
         FROM 
            t_emp e
         INNER JOIN 
-           (SELECT dept_no, AVG(sal) AS avgSal from t_emp GROUP BY dept_no) t
+           (SELECT dept_no, AVG(sal) AS avgSal FROM t_emp GROUP BY dept_no) t
         ON 
-           e.sal>t.avgSal;
+           e.dept_no=t.dept_no AND e.sal>t.avgSal;
 
      3、取得每个部门平均的薪水等级
         SELECT 
-           e.dept_no, s.grade
+           e.dept_no, e.avgSal, s.grade
         FROM 
-           t_emp e
+           (SELECT dept_no, AVG(sal) AS avgSal FROM t_emp GROUP BY dept_no) e
         INNER JOIN 
            sal_grade s
         ON 
-           e.sal between loSal and hiSal
-        GROUP BY 
-           e.dept_no;
+           e.avgSal between s.loSal and s.hiSal
 
      4、不准用（max），取得最高薪水（给出两种解决方案）
         第一种：SELECT sal FROM t_emp ORDER BY sql DESC LIMIT 1;
@@ -124,8 +131,7 @@ MySQL作业题
            d.d_name
         ORDER BY
            grade asc
-        LIMIT
-           1;
+        LIMIT 1;
 
      8、取得比普通员工（员工代码没有在mgr字段上出现的）的最高薪水还要高的领导人姓名.
         SELECT
@@ -152,13 +158,13 @@ MySQL作业题
 
      10、列出所有员工及领导的姓名
          SELECT
-            A.ename AS '员工名',B.ename AS '领导名'
+            a.ename AS '员工名', b.ename AS '领导名'
          FROM
-            t_emp A
+            t_emp a
          LEFT OUTER JOIN
-            t_emp B
+            t_emp b
          ON
-            A.mgr=B.t_emp_no;
+            a.mgr=b.emp_no;
          // 这里选用的是左外连接而没有选用自连接的原因是t_emp表中有一个mgr(领导)为null，选用了自连接就无法显示其领导为null的员工。
 
      11、求出部门名称中，带'S'字符的部门员工的工资合计、部门人数
@@ -173,7 +179,7 @@ MySQL作业题
          WHERE
             d.d_name LIKE '%S%'
          GROUP BY
-            d.dept_no,d.d_name,d.loc
+            d.dept_no, d.d_name, d.loc
 
      12、给员工加薪10%
          UPDATE t_emp SET sal=sal*1.1;
