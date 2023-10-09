0fec077531b20b9252d391818663a623c4284dbb
游诗成 <youshicheng97@163.com>
2023-10-09 16:03:43 +0800

    限流：令牌桶过滤、布隆过滤器

---
+++ SpringBoot的业务技术使用/SpringBoot_Limiting/01_SpringBoot-TokenBucket/src/main/java/com/example/advice/WebControllerAdvice.java
@@ -0,0 +1,16 @@
+package com.example.advice;
+
+import com.example.exception.ApiException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class WebControllerAdvice {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<String> ApiExceptionHandler(ApiException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
