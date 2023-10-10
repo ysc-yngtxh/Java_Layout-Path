4129f29f39bba42fd192515b7ea28af2f2ff87a4
游诗成 <youshicheng97@163.com>
2023-10-10 19:06:29 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud以及前沿技术/14_SpringBoot-Redis/02_SpringBoot-Redis-Serialization/src/test/java/com/example/SpringRedisSerializationTests2.java
+++ SpringCloud以及前沿技术/14_SpringBoot-Redis/02_SpringBoot-Redis-Serialization/src/test/java/com/example/SpringRedisSerializationTests2.java
@@ -19,6 +19,7 @@ public class SpringRedisSerializationTests2 {
     // TODO 问题：在我们使用 RedisTemplate 进行操作内存数据时，我们在 Redis 服务器(使用命令提示符窗口，或者可视化工具) 上可以发现，
     //           不论是中文还是英文，都出现了乱码。
     //      方法二：使用配置类的方式，在项目启动时就对 RedisTemplate操作对象 初始化设置
+    //             所以这时候的 RedisTemplate<Object, Object> 就是我们设置的 RedisTemplate操作对象
     @Autowired
     private RedisTemplate<Object, Object> redisTemplate;
 
