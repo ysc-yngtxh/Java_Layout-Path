6c0e4a1ee690ed7458c8349e0f8da09950baab29
游诗成 <youshicheng97@163.com>
2023-10-15 00:22:28 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/01_Provider-8081/src/main/java/com/example/SpringCloudProviderApplication.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/01_Provider-8081/src/main/java/com/example/SpringCloudProviderApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.mapper")
 public class SpringCloudProviderApplication {
 
     public static void main(String[] args) {
