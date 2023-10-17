f02354f9b604f74596390bb12f32d7d3cd88d6c9
游诗成 <youshicheng97@163.com>
2023-10-17 22:49:26 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/06_Gateway-Config/src/main/java/com/example/GatewayConfigApplication.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/06_Gateway-Config/src/main/java/com/example/GatewayConfigApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudGatewayApplication {
+public class GatewayConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+        SpringApplication.run(GatewayConfigApplication.class, args);
     }
 
 }
