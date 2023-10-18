75565c095e792809f1c8611bfd43eb951b8aa87e
游诗成 <youshicheng97@163.com>
2023-10-18 15:17:59 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/01_Alibaba-Provider/src/main/java/com/example/controller/MyController.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/01_Alibaba-Provider/src/main/java/com/example/controller/MyController.java
@@ -3,12 +3,14 @@ package com.example.controller;
 import com.example.pojo.User;
 import com.example.service.impl.UserServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Map;
 
@@ -25,35 +27,5 @@ public class MyController {
     @GetMapping("/{id}")
     public User query(@PathVariable("id") Integer id){
         return userService.queryById(id);
-    }
-
-    @GetMapping("/header")
-    public String HeaderHandler(HttpServletRequest request) {
-        Enumeration<String> headerNames = request.getHeaderNames();
-        StringBuilder builder = new StringBuilder();
-        while (headerNames.hasMoreElements()) {
-            String nextName = headerNames.nextElement();
-            Enumeration<String> headers = request.getHeaders(nextName);
-            while (headers.hasMoreElements()) {
-                String nextElement = headers.nextElement();
-                if (nextName.equalsIgnoreCase("X-Request-Color") || nextName.equalsIgnoreCase("City")) {
-                    builder.append("<span style=\"color:blue\">").append(nextName).append(": ")
-                            .append(nextElement).append("</span><br/>");
-                } else {
-                    builder.append(nextName).append(": ").append(nextElement).append("<br/>");
-                }
-            }
-        }
-        return builder.toString();
-    }
-
-    @GetMapping("/param")
-    public String ParamHandler(HttpServletRequest request) {
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        StringBuilder builder = new StringBuilder();
-        for (String key : parameterMap.keySet()) {
-
-        }
-        return builder.toString();
     }
 }
