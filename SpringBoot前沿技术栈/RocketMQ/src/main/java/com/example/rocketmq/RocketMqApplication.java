837b2d97368db10652e48f6dcac965105684f22d
游诗成 <youshicheng97@163.com>
2023-10-19 08:29:43 +0800

    提交更新，此次更新无重大版本变革

---
+++ SpringBoot前沿技术栈/RocketMQ/src/main/java/com/example/rocketmq/RocketMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMqApplication.class, args);
+    }
+
+}
