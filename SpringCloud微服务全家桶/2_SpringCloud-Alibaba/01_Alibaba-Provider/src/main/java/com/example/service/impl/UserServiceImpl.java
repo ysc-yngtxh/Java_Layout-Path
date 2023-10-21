62a34d63fa098f7ee42b9398962d6e0305900544
游诗成 <youshicheng97@163.com>
2023-10-22 00:04:20 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/01_Alibaba-Provider/src/main/java/com/example/service/impl/UserServiceImpl.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/01_Alibaba-Provider/src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -18,7 +18,7 @@ public class UserServiceImpl implements UserService {
 
     public User queryById(Integer id){
         /*try {
-            TimeUnit.SECONDS.sleep(2);   // 模拟用户访问超时，这个时候就触发了Hystrix的服务降级
+            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }*/
