3146f1e9d829327718c6cf838a6fef2ddd079255
游诗成 <youshicheng97@163.com>
2023-10-25 17:48:12 +0800

    提交更新，此次更新无重大版本变革

--- Java成神之路/Java基础及进阶/P16_JUC并发/JUC并发Ⅷ_同步机制Volatile及单例详解/B单例模式详解.java
+++ Java成神之路/Java基础及进阶/P16_JUC并发/JUC并发Ⅷ_同步机制Volatile及单例详解/B单例模式详解.java
@@ -26,7 +26,7 @@ public class B单例模式详解 {
     /**
      * 饿汉式单例
      *     类一旦加载就创建一个单例，保证在调用 getInstance 方法之前单例已经存在了
-     *     饿汉式单例在类创建的同时就已经创建好一个静态的对 象供系统使用，以后不再改变，所以是线程安全的，可以直接用于多线程而不会出现问题
+     *     饿汉式单例在类创建的同时就已经创建好一个静态的对象供系统使用，以后不再改变，所以是线程安全的，可以直接用于多线程而不会出现问题
      *     但是缺点就是如果这个类需要的提前就加载好，比较浪费内存空间
      */
     private B单例模式详解(){  
@@ -71,7 +71,7 @@ class LazyMan{
             for (int i = 0; i < 10; i++) {
                 System.out.println( Thread.currentThread().getName() + "--" + LazyMan.getInstance() );
             }
-        }).start();
+        }, "a").start();
 
         new Thread(() -> {
             System.out.println( Thread.currentThread().getName() + "--" + LazyMan.getInstance() );
