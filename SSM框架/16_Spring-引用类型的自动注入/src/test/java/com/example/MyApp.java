643fc535330bab493e3d703619e585dc4f07fe33
游诗成 <youshicheng97@163.com>
2023-10-26 22:53:54 +0800

    提交更新，此次更新无重大版本变革

--- SSM框架/16_Spring-引用类型的自动注入/src/test/java/com/example/MyApp.java
+++ SSM框架/16_Spring-引用类型的自动注入/src/test/java/com/example/MyApp.java
@@ -7,22 +7,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
 
-    //引用类型的自动注入
+    // 引用类型的自动注入
     @Test
     public void test01(){
-
-        //从spring容器中获取UserService类型的对象，dao,service都是spring管理的
+        // 从spring容器中获取UserService类型的对象，dao,service都是spring管理的
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
         Student service = (Student) ac.getBean("myStudent");
 
         System.out.println("Student对象:"+service);
     }
 
-    //多个配置文件的使用
+    // 多个配置文件的使用
     @Test
     public void test02(){
-
-        //从spring容器中获取UserService类型的对象，dao,service都是spring管理的
+        // 从spring容器中获取UserService类型的对象，dao,service都是spring管理的
         ApplicationContext ac = new ClassPathXmlApplicationContext("bao/total.xml");
         Student service = (Student) ac.getBean("myStudent");
 
