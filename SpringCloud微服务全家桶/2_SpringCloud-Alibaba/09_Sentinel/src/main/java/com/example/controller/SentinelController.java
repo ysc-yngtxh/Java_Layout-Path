d39f8a0342c327298ddf7844c2491f2fb0084b03
游诗成 <youshicheng97@163.com>
2024-01-08 13:50:19 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/09_Sentinel/src/main/java/com/example/controller/SentinelController.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/09_Sentinel/src/main/java/com/example/controller/SentinelController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.service.SentinelMethodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class SentinelController {
     private SentinelMethodService userService;
 
     @GetMapping("/getUserById")
-    public String getUserById(Integer Id){
-        return userService.selectUserById(Id);
+    public String getUserById(@RequestParam Integer id){
+        return userService.selectUserById(id);
     }
 }
--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/09_Sentinel/src/main/java/com/example/service/impl/SentinelMethodServiceImpl.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/09_Sentinel/src/main/java/com/example/service/impl/SentinelMethodServiceImpl.java
@@ -4,7 +4,6 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.service.SentinelMethodService;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class SentinelMethodServiceImpl implements SentinelMethodService {
@@ -20,14 +19,15 @@ public class SentinelMethodServiceImpl implements SentinelMethodService {
     // 注意：注解方式埋点不支持 private 方法。
     @SentinelResource(value = RESOURCE_NAME, blockHandler = "selectUserByIdBlockHandler",
             fallback = "selectUserByIdFallback")
-    public String selectUserById(@RequestParam Integer Id) {
+    public String selectUserById(Integer Id) {
         if (Id == 2) {
             throw new RuntimeException("出异常了啊！");
         }
         return "{id: " + Id + ", age: 25}";
     }
 
-    // 超出流量限制的部分是否会进入到blockHandler的方法，要注意是超出流量限制的请求调用，会进入blockHandler方法。
+
+    // 超出流量限制的请求会进入到blockHandler的方法。
     // 注意细节，一定要跟原函数的返回值和形参一致，并且形参最后要加个BlockException参数，否则会报错，FlowException: null
     public String selectUserByIdBlockHandler(Integer Id, BlockException ex) {
         System.out.println("selectUserByIdBlockHandler异常信息：" + ex.getMessage());
