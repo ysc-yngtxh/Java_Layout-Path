50c4688fa78613baaee5c4b2fa9973633ac132f8
游诗成 <youshicheng97@163.com>
2024-01-12 18:26:43 +0800

    提交更新，此次更新无重大版本变革

--- SpringBoot实用业务技术/SpringBoot_访问静态资源/4_SpringBoot-Templates-Controller/src/main/java/com/example/controller/TemplatesController.java
+++ SpringBoot实用业务技术/SpringBoot_访问静态资源/4_SpringBoot-Templates-Controller/src/main/java/com/example/controller/TemplatesController.java
@@ -22,6 +22,7 @@ public class TemplatesController {
         // TODO 有一个细节需要注意：
         //  当你在配置文件中 spring.thymeleaf.prefix=classpath:/templates 时，这里return返回的视图得加斜杠"/login"。
         //  但这样配置 spring.thymeleaf.prefix=classpath:/templates/，就可以直接返回视图名称即可，否则报错。
+        //  最好是在配置文件后面加上斜杠“/”，因为不加上就无法访问到默认欢迎文件index.html了
         return "login";
     }
 
