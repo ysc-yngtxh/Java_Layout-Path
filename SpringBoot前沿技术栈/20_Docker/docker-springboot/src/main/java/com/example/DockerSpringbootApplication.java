241b6810b20c8b564201da49f972860ce688e912
游诗成 <youshicheng97@163.com>
2024-01-16 17:40:19 +0800

    123

--- SpringBoot前沿技术栈/20_Docker/docker-springboot/src/main/java/com/example/DockerSpringbootApplication.java
+++ SpringBoot前沿技术栈/20_Docker/docker-springboot/src/main/java/com/example/DockerSpringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.example.mapper")
+@MapperScan(basePackages = "com.example.dao")
 public class DockerSpringbootApplication {
 
 	public static void main(String[] args) {
