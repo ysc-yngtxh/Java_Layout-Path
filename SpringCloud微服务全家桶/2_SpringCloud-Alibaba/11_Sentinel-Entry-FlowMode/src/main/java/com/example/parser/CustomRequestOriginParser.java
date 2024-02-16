0e60a4e5d54a30ee06bcff4596437b2352e08ebb
游诗成 <youshicheng97@163.com>
2024-02-16 21:46:30 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/11_Sentinel-Entry-FlowMode/src/main/java/com/example/parser/CustomRequestOriginParser.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/11_Sentinel-Entry-FlowMode/src/main/java/com/example/parser/CustomRequestOriginParser.java
@@ -8,15 +8,15 @@ import org.springframework.util.StringUtils;
 /**
  * @author 游家纨绔
  * @dateTime 2024-02-01 16:09
- * @apiNote TODO 定义流控中的请求来源
+ * @apiNote TODO 解析流控中的请求来源
  */
 @Component
-public class DepartRequestOriginParser implements RequestOriginParser {
+public class CustomRequestOriginParser implements RequestOriginParser {
     // 假设请求来源标识是以请求参数的形式出现
-    // Sentinel会自动根据parserOrigin()方法的返回结果来判断请求源
+    // Sentinel会自动根据parserOrigin()方法将请求解析，并且将方法返回结果设置为请求来源
     @Override
     public String parseOrigin(HttpServletRequest httpServletRequest) {
-        // 获取请求参数source的值，即请求源标识
+        // 获取请求参数name的值，即请求源标识
         String name = httpServletRequest.getParameter("name");
         // 若请求中没有携带name请求参数，则默认请求源为ysc
         if (!StringUtils.hasText(name)) {
---
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/12_Sentinel-Authorization-ParamMode/src/main/java/com/example/service/impl/SentinelServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.service.impl;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.example.service.SentinelService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 游家纨绔
+ * @dateTime 2024-01-10 16:07
+ * @apiNote TODO
+ */
+@Service
+public class SentinelServiceImpl implements SentinelService {
+
+    public static final String GET_HANDLER = "handler";
+    public static final String GET_LIST = "list";
+    public static final String GET_INFO = "info";
+
+    // 该`GET_HANDLER`资源具有流控规则
+    @SentinelResource(value = GET_HANDLER, blockHandler = "getHandlerBlockHandler")
+    public String getHandler() {
+        return "定义了 getHandler 在业务层的资源名！！！";
+    }
+
+    public String getHandlerBlockHandler(BlockException ex) {
+        System.out.println("getHandlerBlockHandler异常信息：" + ex.getMessage());
+        return "{code: 500, msg: 服务流量限流处理}";
+    }
+
+
+    @SentinelResource(value = GET_LIST, blockHandler = "getListBlockHandler")
+    public String getList(Integer id, String username) {
+        return "定义了 getList 在业务层的资源名！！！";
+    }
+
+    public String getListBlockHandler(Integer id, String username, BlockException ex) {
+        System.out.println("getListsBlockHandler异常信息：" + ex.getMessage());
+        return "{code: 500, msg: 服务流量限流处理}";
+    }
+}
