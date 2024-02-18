dd4646c604341f2409ea75a8a90559c037b8b084
游诗成 <youshicheng97@163.com>
2024-02-18 12:21:14 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/05_OpenFeign-Nacos/src/main/java/com/example/ConsumerOpenFeignApplication.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/05_OpenFeign-Nacos/src/main/java/com/example/ConsumerOpenFeignApplication.java
@@ -1,8 +1,10 @@
 package com.example;
 
 import com.example.config.OpenFeignConfig;
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 
 @LoadBalancerClients(defaultConfiguration = OpenFeignConfig.class) // 设置LoadBalance负载均衡策略
 @EnableFeignClients  // 开启OpenFeign客户端
+@EnableDiscoveryClient(autoRegister = false) // 关闭自动注册到注册中心Nacos，默认为true
+// 换句话说，如果我们不想该服务注册到注册中心，只是想从注册中心拉取服务，就可以设置 (autoRegister = false)
 @SpringBootApplication
 public class ConsumerOpenFeignApplication {
 
@@ -18,6 +22,13 @@ public class ConsumerOpenFeignApplication {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    // 开启并配置OpenFeign的日志级别
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        // FULL是详细日志
+        return Logger.Level.FULL;
     }
 
     public static void main(String[] args) {
