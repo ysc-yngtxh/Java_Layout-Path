a9368b13f98865caec2f94ec063ecd3dd4df2b3a
游诗成 <youshicheng97@163.com>
2024-02-19 12:33:12 +0800

    提交更新，此次更新无重大版本变革

--- SpringBoot实用业务技术/SpringBoot-SPI/src/main/java/com/example/SpringBootSpiApplication.java
+++ SpringBoot实用业务技术/SpringBoot-SPI/src/main/java/com/example/SpringBootSpiApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
 import com.example.spi.Robot;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,7 +25,11 @@ public class SpringBootSpiApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // JDK的SPI机制
+        // TODO JDK的SPI机制
+        //      缺陷：1、不能按需加载：需要遍历所有实现并实例化，然后在循环中找到我们需要的实现。如果不想用某些实现类，
+        //                         或者某些类实例化很耗时，它也被载入并实例化了，就会造成浪费
+        //           2、获取某个实现类的方式不够灵活：只能通过迭代器形式获取，不能根据某个参数来获取对应的实现类
+        //           3、多个并发多线程使用ServiceLoader类的实例是不安全的
         ServiceLoader<Robot> serviceLoader = ServiceLoader.load(Robot.class);
         // serviceLoader.forEach(Robot::sayHello);
         Iterator<Robot> iterator = serviceLoader.iterator();
@@ -36,8 +41,20 @@ public class SpringBootSpiApplication implements CommandLineRunner {
 
         System.out.println("=======================================================");
 
-        // Spring的SPI机制
+        // TODO Spring SPI沿用了Java SPI的设计思想，Spring采用的是‘spring.factories’方式实现SPI机制，
+        //      可以在不修改Spring源码的前提下，提供Spring框架的扩展性
+        //      缺陷：和Java SPI一样，Spring SPI也无法获取某个固定的实现，只能按顺序获取所有实现
+        // List<Robot> robots = SpringFactoriesLoader.loadFactories(Robot.class, Thread.currentThread().getContextClassLoader());
         List<Robot> robots = SpringFactoriesLoader.loadFactories(Robot.class, null); // 第二个参数为null表示使用当前的类加载器
         robots.forEach(Robot::sayHello);
+
+        System.out.println("=======================================================");
+
+        // TODO Dubbo SPI 除了支持按需加载接口实现类，还增加了 IOC和AOP 等特性
+        ExtensionLoader<Robot> extensionLoader = ExtensionLoader.getExtensionLoader(Robot.class);
+        Robot bumblebee = extensionLoader.getExtension("bumblebee");
+        Robot optimusPrime = extensionLoader.getExtension("optimusPrime");
+        bumblebee.sayHello();
+        optimusPrime.sayHello();
     }
 }
--- SpringBoot实用业务技术/SpringBoot-SPI/src/main/java/com/example/spi/Robot.java
+++ SpringBoot实用业务技术/SpringBoot-SPI/src/main/java/com/example/spi/Robot.java
@@ -1,10 +1,13 @@
 package com.example.spi;
 
+import com.alibaba.dubbo.common.extension.SPI;
+
 /**
  * @author 游家纨绔
  * @dateTime 2024-02-19 08:41
  * @apiNote TODO
  */
+@SPI  // TODO 注意⚠️：只有使用 Dubbo SPI 时才需要该@SPI注解
 public interface Robot {
     void sayHello();
 }
