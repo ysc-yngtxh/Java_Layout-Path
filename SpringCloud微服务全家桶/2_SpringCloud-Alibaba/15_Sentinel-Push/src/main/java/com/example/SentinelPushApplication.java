44a8801ebb4ff001c6c05d2820d584f3104d9e48
游诗成 <youshicheng97@163.com>
2024-02-19 23:05:38 +0800

    提交更新，此次更新无重大版本变革

--- SpringCloud微服务全家桶/2_SpringCloud-Alibaba/15_Sentinel-Push/src/main/java/com/example/SentinelPushApplication.java
+++ SpringCloud微服务全家桶/2_SpringCloud-Alibaba/15_Sentinel-Push/src/main/java/com/example/SentinelPushApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSentinelPushApplication {
+public class SentinelPushApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSentinelPushApplication.class, args);
+        SpringApplication.run(SentinelPushApplication.class, args);
     }
 
 }
