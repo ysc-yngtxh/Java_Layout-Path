e542761dc5a15241e8f3fc5fb3145292b8d1f833
游家纨绔 <youshicheng97@163.com>
2024-06-08 20:13:59 +0800

    代码提交，无重大版本更迭

---
+++ 源码分析/RPC/v3/RPC-FrameWork3/src/main/java/com/example/loadbalance/LoadBalance.java
@@ -0,0 +1,20 @@
+package com.example.loadbalance;
+
+import com.example.common.URL;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author 游家纨绔
+ * @dateTime 2024-04-08 上午11:04
+ * @apiNote TODO
+ */
+public class LoadBalance {
+
+    public static URL random(List<URL> urlList){
+        Random random = new Random();
+        int nextInt = random.nextInt(urlList.size());
+        return urlList.get(nextInt);
+    }
+}
