e43e019bf24c7daf75d46d03713a9ed9cc278407
游家纨绔 <youshicheng97@163.com>
2024-07-01 05:46:29 +0800

    代码提交，无重大版本更迭

---
+++ SpringBoot实用业务技术/SpringBoot_Druid/Druid详情.md
@@ -0,0 +1,33 @@
+### 一、前言
+
+###### Druid的简介
+
+Druid首先是一个数据库连接池。Druid是目前最好的数据库连接池，
+在功能、性能、扩展性方面，都超过其他数据库连接池，包括DBCP、C3P0、BoneCP、Proxool、JBoss DataSource。
+Druid已经在阿里巴巴部署了超过600个应用，经过一年多生产环境大规模部署的严苛考验。
+Druid是阿里巴巴开发的号称为监控而生的数据库连接池！
+
+###### Druid的功能
+
+1、替换DBCP和C3P0。Druid提供了一个高效、功能强大、可扩展性好的数据库连接池。
+
+2、可以监控数据库访问性能，Druid内置提供了一个功能强大的StatFilter插件，
+   能够详细统计SQL的执行性能，这对于线上分析数据库访问性能有帮助。
+
+3、数据库密码加密。直接把数据库密码写在配置文件中，这是不好的行为，容易导致安全问题。
+   DruidDruiver和DruidDataSource都支持PasswordCallback。
+
+4、SQL执行日志，Druid提供了不同的LogFilter，能够支持Common-Logging、Log4j和JdkLog，
+   可以按需要选择相应的LogFilter，监控你应用的数据库访问情况。
+
+5、扩展JDBC，如果你要对JDBC层有编程的需求，可以通过Druid提供的Filter机制，很方便编写JDBC层的扩展插件。
+
+所以Druid可以：
+1、充当数据库连接池。
+2、可以监控数据库访问性能
+3、获得SQL执行日志
+
+> 更多可参考官方文档：[https://github.com/alibaba/druid/](https://github.com/alibaba/druid/)
+
+#### 4、访问 [http://127.0.0.1:8080/druid/index.html](http://127.0.0.1:8080/druid/index.html) 查看监控信息
+
---
+++ SpringBoot实用业务技术/SpringBoot_Druid/src/main/java/com/example/config/DataSourceConfig.java
@@ -0,0 +1,68 @@
+package com.example.config;
+
+import com.alibaba.druid.spring.boot3.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
+import javax.sql.DataSource;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class DataSourceConfig {
+
+    /**
+     * 添加 DruidDataSource 组件到容器中，并绑定属性
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConditionalOnProperty(name = "spring.datasource.type", havingValue = "com.alibaba.druid.pool.DruidDataSource")
+    public DataSource dataSource(){
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    /**
+     * 配置事物管理器
+     */
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    /**
+     * ↓↓↓↓↓↓  配置spring监控  ↓↓↓↓↓↓
+     * DruidStatInterceptor: druid提供的拦截器
+     */
+    @Bean
+    public DruidStatInterceptor druidStatInterceptor() {
+        return new DruidStatInterceptor();
+    }
+
+    /**
+     * 使用正则表达式配置切点
+     */
+    @Bean
+    @Scope("prototype")
+    public JdkRegexpMethodPointcut druidStatPointcut() {
+        JdkRegexpMethodPointcut pointcut = new JdkRegexpMethodPointcut();
+        pointcut.setPattern("com.zhengqing.demo.modules.*.api.*");
+        return pointcut;
+    }
+
+    /**
+     * DefaultPointcutAdvisor类定义advice及 pointcut 属性。
+     * advice指定使用的通知方式，也就是druid提供的DruidStatInterceptor类，pointcut指定切入点
+     */
+    @Bean
+    public DefaultPointcutAdvisor druidStatAdvisor(DruidStatInterceptor druidStatInterceptor, JdkRegexpMethodPointcut druidStatPointcut) {
+        DefaultPointcutAdvisor defaultPointAdvisor = new DefaultPointcutAdvisor();
+        defaultPointAdvisor.setPointcut(druidStatPointcut);
+        defaultPointAdvisor.setAdvice(druidStatInterceptor);
+        return defaultPointAdvisor;
+    }
+}
