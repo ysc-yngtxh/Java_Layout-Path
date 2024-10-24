de928039254c7a42b0f2a44e7fdf43e728b35710
游家纨绔 <youshicheng97@163.com>
2024-10-24 22:52:51 +0800

    代码提交，无重大版本更迭

--- SpringBoot前沿技术栈/17_SpringBoot-Email/src/test/java/com/example/EmailApplicationTests.java
+++ SpringBoot前沿技术栈/17_SpringBoot-Email/src/test/java/com/example/EmailApplicationTests.java
@@ -6,7 +6,6 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
