995a33207a3149f52b2f369df5cca73b944d482e
游家纨绔 <youshicheng97@163.com>
2024-11-16 00:31:59 +0800

    代码提交，无重大版本更迭

--- SpringBoot前沿技术栈/06_SpringSecurity/06.7_Spring-Security-OAuth2/06.7_3-Custom-OAuth2/OAuth2-Resource-Server/src/main/java/com/example/api/UserController.java
+++ SpringBoot前沿技术栈/06_SpringSecurity/06.7_Spring-Security-OAuth2/06.7_3-Custom-OAuth2/OAuth2-Resource-Server/src/main/java/com/example/api/UserController.java
@@ -3,17 +3,17 @@ package com.example.api;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 
     @RequestMapping(path = "/user/info", method = {RequestMethod.GET, RequestMethod.POST}, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public Map<String, Object> getUser() {
+    public @ResponseBody Map<String, Object> getUser() {
         Map<String, Object> map = new HashMap<>();
         map.put("name", "xxx");
         return map;
