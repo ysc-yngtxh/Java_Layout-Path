62a9df35d31108df1099c1d6e8220c2c926547e8
游家纨绔 <youshicheng97@163.com>
2024-12-02 00:06:20 +0800

    代码提交，无重大版本更迭

--- SpringBoot前沿技术栈/06_SpringSecurity/06.7_Spring-Security-OAuth2/06.7_4-Custom-SpringAuthorizationServer/demo-server/src/main/java/com/example/config/SecurityConfig.java
+++ SpringBoot前沿技术栈/06_SpringSecurity/06.7_Spring-Security-OAuth2/06.7_4-Custom-SpringAuthorizationServer/demo-server/src/main/java/com/example/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,18 +23,19 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(1)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         // 授权服务器的安全交给 SpringSecurity 的过滤器处理
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         // 自定义授权服务器的配置
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                // 自定义授权同意页
+                // 自定义授权同意页（解决 OAuth2 因网络原因导致授权同意页刷新缓慢的问题）
                 .authorizationEndpoint(auth -> auth
                         .consentPage("/consent")
                 )
                 .oidc(Customizer.withDefaults());    // Initialize `OidcConfigurer`
 
-        // 将未认证的请求重定向到 /login
+        // 配置异常处理器：将未认证的异常请求重定向到 /login
         http
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(
@@ -45,6 +47,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(2)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
