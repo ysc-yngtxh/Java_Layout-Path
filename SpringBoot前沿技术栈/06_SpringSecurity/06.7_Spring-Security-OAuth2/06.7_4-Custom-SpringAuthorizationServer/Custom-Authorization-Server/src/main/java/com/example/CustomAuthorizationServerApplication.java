9230da4fae0bcdaee82e7a821ab6e6cade129ac3
游家纨绔 <youshicheng97@163.com>
2024-12-03 23:56:41 +0800

    代码提交，无重大版本更迭

---
+++ SpringBoot前沿技术栈/06_SpringSecurity/06.7_Spring-Security-OAuth2/06.7_4-Custom-SpringAuthorizationServer/Custom-Authorization-Server/src/main/java/com/example/CustomAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomAuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ SpringBoot前沿技术栈/06_SpringSecurity/06.7_Spring-Security-OAuth2/06.7_6-OIDC-SpringAuthorizationServer/OIDC-Authorization-Server/src/main/java/com/example/OIDCAuthorizationServerApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OIDCAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OIDCAuthorizationServerApplication.class, args);
+    }
+
+    // UserInfo 端点是在 OIDC 协议中定义的、用于安全暴露用户数据给客户端的接口，在本系列第21集"保存
+    // OAuth2用户信息”中曾经讲到过，
+    // 但是，当时所开发的客户端应用是基于 Gitee 授权服务器的，而 Gitee 采用的是标准OAuth2协议，
+    // 所以客户端可以直接携带 access_token 请求用户信息，但这种方式并不适用于SpringAuthorization Server。
+
+}
