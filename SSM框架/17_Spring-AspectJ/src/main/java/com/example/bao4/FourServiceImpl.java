0283058faa6702a769576174f5322d0f69a3c9bd
游家纨绔 <youshicheng97@163.com>
2025-01-14 04:26:31 +0000

    update SSM框架/17_Spring-AspectJ/src/main/java/com/example/bao4/FourServiceImpl.java.
    Signed-off-by: 游家纨绔 <youshicheng97@163.com>

--- SSM框架/17_Spring-AspectJ/src/main/java/com/example/bao4/FourServiceImpl.java
+++ SSM框架/17_Spring-AspectJ/src/main/java/com/example/bao4/FourServiceImpl.java
@@ -5,6 +5,6 @@ public class FourServiceImpl implements FourService {
     @Override
     public String like(String name, Integer time) {
         System.out.println("=====like()方法的执行====");
-        return "曹玉敏，我喜欢你!";
+        return "小曹，我like你!";
     }
 }
