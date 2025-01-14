a0d7a2827a12228ef9bb58e689f4f8c74c8e4cf1
游家纨绔 <youshicheng97@163.com>
2025-01-14 04:06:57 +0000

    update SpringBoot/07_SpringBoot-Aop-Proxy/src/main/java/com/example/SpringBootAopApplication.java.
    Signed-off-by: 游家纨绔 <youshicheng97@163.com>

--- SpringBoot/07_SpringBoot-Aop-Proxy/src/main/java/com/example/SpringBootAopApplication.java
+++ SpringBoot/07_SpringBoot-Aop-Proxy/src/main/java/com/example/SpringBootAopApplication.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 // 开启AOP
 // proxyTargetClass：默认值为false，表示使用JDK动态代理。将其设置为true，表示不论有没有接口都使用CGLIB动态代理。
-// exposeProxy：默认值为false，表示不暴露代理对象。将其设置为true，表示暴露代理对象。即可以对当前类中调用方法进行AOP。
+// exposeProxy：默认值为false，表示不暴露代理对象。
+//              将其设置为true，表示强制AOP代理创建器暴露代理对象为ThreadLocal模式。使其能在被代理的对象内部通过AopContext访问。
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 // TODO 坑点：
 //      Spring 框架中，默认使用的是 JDK代理；如果希望使用的是 CGLIB代理，只需修改属性值 proxyTargetClass = true。
