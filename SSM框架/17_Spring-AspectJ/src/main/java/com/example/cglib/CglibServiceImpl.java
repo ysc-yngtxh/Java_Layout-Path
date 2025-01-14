4578a7a2e9d394e59e49c4280403b15ed90a127b
游家纨绔 <youshicheng97@163.com>
2025-01-14 04:26:59 +0000

    update SSM框架/17_Spring-AspectJ/src/main/java/com/example/cglib/CglibServiceImpl.java.
    Signed-off-by: 游家纨绔 <youshicheng97@163.com>

--- SSM框架/17_Spring-AspectJ/src/main/java/com/example/cglib/CglibServiceImpl.java
+++ SSM框架/17_Spring-AspectJ/src/main/java/com/example/cglib/CglibServiceImpl.java
@@ -7,6 +7,6 @@ public class CglibServiceImpl {
 
     public String like(String name, Integer time) {
         System.out.println("=====like()方法的执行====");
-        return "曹玉敏，我喜欢你!";
+        return "小曹，我like你!";
     }
 }
