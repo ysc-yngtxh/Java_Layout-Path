30edb62026d96614ba207806188ae0f81e083203
游家纨绔 <youshicheng97@163.com>
2025-03-04 16:37:06 +0800

    代码提交，无重大版本更迭

---
+++ SpringBoot前沿技术栈/01_ELK/LogStash/src/main/java/com/example/LogStashApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogStashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogStashApplication.class, args);
+	}
+
+}
