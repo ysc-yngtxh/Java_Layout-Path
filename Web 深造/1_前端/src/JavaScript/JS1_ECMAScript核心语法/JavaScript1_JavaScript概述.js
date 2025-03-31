218670b27d9d63cc6186fb7922389ad93bbf9448
游家纨绔 <youshicheng97@163.com>
2025-03-31 17:27:02 +0800

    代码提交，无重大版本更迭

---
+++ Web 深造/1_前端/src/JavaScript/JS1_ECMAScript核心语法/JavaScript1_JavaScript概述.js
@@ -0,0 +1,21 @@
+/*
+  1、什么是Java Script，有什么用？
+        Java script 是运行在浏览器上的脚本语言，简称JS
+        Java script 是网景公司(NetScape)的布兰登艾奇(javascript之父)开发的，最初叫做LiveScript.
+        LiveScript 的出现让浏览器更加的生动了，不再是单纯的静态页面了，页面更具有交互性。
+        javaScript 这个名字虽然带有“Java”但是和Java没任何关系，只是语法上有点类似。
+        他们运行的位置不同，java运行在JVM当中，javaScript运行在浏览器的内存当中
+
+        JavaScript 程序不需要我们程序员手动编译，编写完源代码之后，浏览器直接打开解释执行。
+        JavaScript 的“目标程序”以普通文本形式保存，这种语言都叫作“脚本语言”。
+
+        Java的目标程序以.class形式存在，不能使用文本编译器打开，不是脚本语言。
+
+        以后会学习一个叫做JSP的技术，JSP和JS有啥区别？
+            JSP：Java Server Pages(隶属于Java语言，运行在JVM当中)
+            JS：JavaScript(运行在浏览器上)
+ */
+
+window.alert("hello js!");
+window.alert("hello js text!");
+window.alert("hello js only!");
