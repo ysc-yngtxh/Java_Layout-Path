ddd29a3388667d2831a70b1162de88c3b65ca08a
游家纨绔 <youshicheng97@163.com>
2025-05-27 19:12:19 +0800

    代码提交，无重大版本更迭

---
+++ SpringBoot实用业务技术/SpringBoot_响应式编程-Event事件监听器/02_SpringBoot-Reactor-Annotation/src/main/java/com/example/ReactorSpringBootApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorSpringBootApplication.class, args);
+	}
+
+	/** 注解式驱动编程模型
+	 *
+	 * SpringWebflux请求和响应不再是ServletRequest和ServletResponse，而是ServerRequest和ServerResponse。
+	 *
+	 * Flux 用于表示多个数据的流，而 Mono 用于表示单个数据。
+	 */
+}
