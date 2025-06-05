f8132e1f1a321574941e3cbaa315598563ecebe4
游家纨绔 <youshicheng97@163.com>
2025-06-05 18:48:53 +0800

    代码提交，无重大版本更迭

--- SpringBoot前沿技术栈/05_SpringBoot-Cache/05.5_SpringBoot-Redisson/src/main/java/com/example/service/RedissonService.java
+++ SpringBoot前沿技术栈/05_SpringBoot-Cache/05.5_SpringBoot-Redisson/src/main/java/com/example/service/RedissonService.java
@@ -23,24 +23,30 @@ public class RedissonService {
 
 	// 分布式锁
 	public void lock(String lockName, long waitTime) {
-		// 参数(lockName)为锁的名称，作为Redis中的key
+		// 参数(lockName)为锁的名称，作为Redis中的key。getLock() 方法获取一个分布式锁对象。
 		RLock rLock = redissonClient.getLock(lockName);
 		try {
-			// tryLock() 方法尝试获取锁。如果在指定时间内获取到锁返回true，否则返回false
-			// 参数一：等待获取锁的最大时间（毫秒）
-			// 参数二：锁的自动释放时间（leaseTime，毫秒）
-			// 参数三：时间单位
-			boolean isLocked = rLock.tryLock(waitTime, 30, TimeUnit.MILLISECONDS);
+			System.out.println("当前线程所持有锁是否被锁定：" + rLock.isLocked());  // 检查是否被锁定
+			// rLock.lock();  // 获取锁，默认阻塞等待，直到获取到锁为止
 
-			// 当没有显式设置 leaseTime参数(即第二个参数)，看门狗即可生效，否则不生效。
-			// boolean isLocked2 = rLock.tryLock();
-			// boolean isLocked3 = rLock.tryLock(waitTime, TimeUnit.MILLISECONDS);
+			// tryLock() 方法尝试获取锁。
+			// 参数一：等待获取锁的最大时间（毫秒）、参数二：锁的自动释放时间（leaseTime，毫秒）、参数三：时间单位
+			// 如果在指定时间(waitTime)内获取到锁返回true，否则返回false。且获取到锁后只有3秒有效时长，超过3秒会释放锁。
+			boolean acquired = rLock.tryLock(waitTime, 3000, TimeUnit.MILLISECONDS);
 
-			if (isLocked) {
+			// 看门狗生效前提：1、线程已经成功获取锁。 2、未指定 leaseTime参数(即第二个参数)
+			// 注意⚠️：当前线程获取锁后，Redisson会自动开启看门狗机制，定时续期锁的过期时间。
+			//        如果在指定时间（waitTime）内获取不到锁，则不会开启看门狗机制。
+			// boolean acquired2 = rLock.tryLock();
+			// boolean acquired3 = rLock.tryLock(waitTime, TimeUnit.MILLISECONDS);
+
+			if (acquired) {
+				System.out.println(rLock.remainTimeToLive()); // 获取锁的剩余时间（毫秒）
 				// TODO 业务逻辑
 			}
 		} catch (Exception e) {
-			if (rLock.isHeldByCurrentThread()) {
+			// 检查当前线程是否持有锁、锁是否被锁定
+			if (rLock.isLocked() && rLock.isHeldByCurrentThread()) {
 				rLock.unlock();
 			}
 		}
