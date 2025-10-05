663fb8ed1bab25d42829663b42e014344f6dde8c
游家纨绔 <youshicheng97@163.com>
2025-10-05 14:00:51 +0800

    代码提交，无重大版本更迭

--- SpringBoot/32_SpringBoot-Data-Jpa/32.2_OneMany-CriteriaAPI/src/main/java/com/example/repository/TeacherRepository.java
+++ SpringBoot/32_SpringBoot-Data-Jpa/32.2_OneMany-CriteriaAPI/src/main/java/com/example/repository/TeacherRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-// JpaRepository 用于简单查询，传入泛型，第一个参数为要操作的实体类，第二个参数为该实体类的主键类型
+// JpaRepository            用于简单查询，传入泛型，第一个参数为要操作的实体类，第二个参数为该实体类的主键类型
 // JpaSpecificationExecutor 可用于动态生成query，提供了一个高级的入口可以使用底层Jpa的Criteria的所有方法，用以满足所有业务场景
 public interface TeacherRepository extends JpaRepository<Teacher, Integer>, JpaSpecificationExecutor<Teacher> {}
