3ad0954797165738d6d3a161f21112f61ef8d6e0
游家纨绔 <youshicheng97@163.com>
2025-12-20 23:39:45 +0800

    代码提交，无重大版本更迭

---
+++ SpringBoot前沿技术栈/24_SpringBoot-WebFlux/src/main/java/com/example/config/R2dbcConfig.java
@@ -0,0 +1,35 @@
+package com.example.config;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.core.DatabaseClient;
+
+@Configuration
+@EnableR2dbcRepositories
+public class R2dbcConfig {
+    
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.r2dbc")
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get(ConnectionFactoryOptions.parse("r2dbc:postgresql://localhost:5432/mydb")
+                                                               .mutate()
+                                                               .option(ConnectionFactoryOptions.USER, "postgres")
+                                                               .option(ConnectionFactoryOptions.PASSWORD, "password")
+                                                               .build());
+    }
+    
+    @Bean
+    @Primary
+    public DatabaseClient databaseClient(ConnectionFactory connectionFactory) {
+        return DatabaseClient.builder()
+                             .connectionFactory(connectionFactory)
+                             .build();
+    }
+}
