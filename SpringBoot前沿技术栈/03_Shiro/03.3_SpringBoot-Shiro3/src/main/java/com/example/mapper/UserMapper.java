82c5b955cfd1de71942053f63dcc7dc4db5120b8
游家纨绔 <youshicheng97@163.com>
2026-08-22 23:39:16 +0800

    代码提交，无重大版本更迭

---
+++ SpringBoot前沿技术栈/03_Shiro/03.3_SpringBoot-Shiro3/src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.mapper;
+
+import com.example.pojo.User;
+
+/**
+ * @Entity generator.pojo.Students
+ */
+public interface UserMapper {
+
+    User selectByPrimaryKey(Long id);
+
+    User selectByName(String name);
+}
